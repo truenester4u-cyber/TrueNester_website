ce014eae35473f405287588f47f8f54c44396376
faiankh02 <heyfaizan02@gmail.com>
2026-03-10 15:39:35 +0530

    fix: correct showProjects toggle logic - show FeaturedProperties when enabled, hide when disabled

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -24,7 +24,7 @@ const Index = () => {
       {showRentals && <JumeirahRentals />}
       <ReviewsSection />
       <PropertyTypes />
-      {!showProjects && <FeaturedProperties />}
+      {showProjects && <FeaturedProperties />}
       <LocationsGrid />
       <AbuDhabiLocationsGrid />
       <RasAlKhaimahLocationsGrid />
